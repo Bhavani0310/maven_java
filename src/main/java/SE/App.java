87de2431c9a97fa218fca50ea1cdf8a14a98af25
Bhavani Prasad <91059918+Bhavani0310@users.noreply.github.com>
2024-01-26 13:15:28 +0530

    checking poll SCM

--- src/main/java/SE/App.java
+++ src/main/java/SE/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hello,Bhavani Prasad");
+        System.out.println("Hello,Prasad");
         System.out.println("Hii");
     }
 }
